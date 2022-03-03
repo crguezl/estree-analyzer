a8f03dc034df9763e30462f0cc9c841cacb1e314
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-03 09:03:01 +0000

    added example

---
+++ examples/hello-world.js
@@ -0,0 +1,21 @@
+const acorn = require('acorn');
+//const analyzer = require('estree-analyzer');
+const analyzer = require('../lib/index.js');
+
+const expressions = [
+    `'1 + 2 * 3 = ' + (1 + 2 * 3)`, 
+`null`, 
+`[1,2,3]`, 
+`["hi", "world"]`];
+
+expressions.forEach(code => {
+    let expr = acorn.parseExpressionAt(code);
+    let analysis = analyzer.analyze(expr);
+    console.log(`${code} has type: ${JSON.stringify(analysis, null, 0)}`);
+})
+
+let code = `a && a.nested && a.nested.prop`;
+let expr = acorn.parseExpressionAt(code);
+let scope = new analyzer.Scope();
+analyzer.analyze(expr, scope);
+console.log(`scope.members for '${code}' is:\n${JSON.stringify(scope.members, null, 2)}`);
