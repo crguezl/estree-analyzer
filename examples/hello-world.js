cdcae75ec9f4b74c7bb54f663895fb739b7459b7
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-03 14:15:40 +0000

    examples/hello-world.js

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -1,17 +1,21 @@
 import * as acorn from 'acorn'
 //const analyzer = require('estree-analyzer');
-const analyzer = require('../lib/index.js');
+import * as analyzer  from  '../src/index.mjs';
+//const acorn = require('acorn');
+//const analyzer = require('../lib/index.js');
+
+//const analyzer = require('estree-analyzer');
 
 const expressions = [ `'1 + 2 * 3 = ' + (1 + 2 * 3)`, `null`, `[1,2,3]`, `["hi", "world"]`];
 
 expressions.forEach(code => {
-    let expr = acorn.parseExpressionAt(code);
+    let expr = acorn.parseExpressionAt(code, 0, { ecmaVersion: 2022});
     let analysis = analyzer.analyze(expr);
     console.log(`${code} has type: ${JSON.stringify(analysis, null, 0)}`);
 })
 
 let code = `a && a.nested && a.nested.prop`;
-let expr = acorn.parseExpressionAt(code);
+let expr = acorn.parseExpressionAt(code, 0, { ecmaVersion: 2022});
 let scope = new analyzer.Scope();
 analyzer.analyze(expr, scope);
 console.log(`scope.members for '${code}' is:\n${JSON.stringify(scope.members, null, 2)}`);
